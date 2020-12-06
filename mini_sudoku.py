eac4d70c5318a376948c341db3f07c5b71e17526
Ilya Chuharev <ichuxarew@mail.ru>
2020-12-06 19:22:05 +0300

    first commit

---
+++ mini_sudoku.py
@@ -0,0 +1,4 @@
+def any_duplicates(square):
+    plain = [i for x in square for i in x]
+    return sorted(plain) != [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    any_duplicates = ([1, 3, 2], [9, 7, 8], [4, 5, 6])
